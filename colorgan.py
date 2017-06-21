bbfd68d576ceb9e8d6dfcd29905692f262ec0dc1
LJQ <l.j.q.light@gmail.com>
2017-06-21 15:49:15 +0800

    Add files via upload

--- colorgan.py
+++ colorgan.py
@@ -1,6 +1,7 @@
 ﻿import ljqpy, os, sys, time, random
 import numpy as np
 import keras.backend as K
+import tensorflow as tf
 from collections import defaultdict
 from keras.datasets import mnist
 from keras.utils.generic_utils import Progbar
@@ -12,17 +13,17 @@ from PIL import Image
 import cv2
 time.clock()
 
-np.random.seed(1333)
+np.random.seed(1335)
 K.set_image_dim_ordering('tf')
-
 from model import BuildGenerator, BuildDiscriminator
 
 # params
-nb_epochs = 500
+nb_epochs = 100
 batch_size = 1
 p_wgangp = 10
+p_recon = 10
 p_gray = 10
-p_beta = 10
+p_diff = 1
 
 channels = 3
 imgsize = 256
@@ -36,10 +37,10 @@ testimgdirA = '/mnt/smb25/ImageNet/erciyuan/test'
 modeldir   = 'data/'
 testimgdir = 'images/'
 
-
+colorweight = np.array([[[[0.29891, 0.58661, 0.11448]]]]) * 3
 def imgloss(y_true, y_pred):
 	diff = K.abs(y_true - y_pred)
-	diff *= [[[[0.29891, 0.58661, 0.11448]]]]
+	diff *= colorweight
 	return K.mean(diff)
 
 def RLinearMerge(x):
@@ -53,22 +54,20 @@ def GradientsPenalty(x):
 	return gradp
 
 def wloss(y_true, y_pred):
-	return - K.mean(y_pred)
-
+	return - K.clip(K.mean(y_pred), -50, 50)
 
 img = Input(shape=(imgsize,imgsize,1))
 modelG = BuildGenerator(img)
 img = Input(shape=(imgsize,imgsize,3))
 modelD = BuildDiscriminator(img)
 
-modelG.summary()
-modelD.summary()
-
 try:
 	modelG.load_weights(os.path.join(modeldir, 'modelG.h5'))
 	modelD.load_weights(os.path.join(modeldir, 'modelD.h5'))	
 except Exception as e: 
 	print(e); print('new model')
+	modelG.summary()
+	modelD.summary()
 
 modelD.compile(optimizer=Adam(adam_lr, adam_beta_1), loss='mse')
 modelG.compile(optimizer=Adam(adam_lr, adam_beta_1), loss='mse')
@@ -77,32 +76,44 @@ imageReal = Input(shape=(imgsize,imgsize,channels))
 imageFake = Input(shape=(imgsize,imgsize,channels))
 DReal = modelD(imageReal)
 DFake = modelD(imageFake)
-inter = merge([imageReal, imageFake], mode=RLinearMerge, output_shape=lambda d:d[0])
-gradp = merge([inter, modelD(inter)], mode=GradientsPenalty, output_shape=lambda d:(d[0],1))
-combD = Model(inputs=[imageReal, imageFake], outputs=[DReal, DFake, gradp])
-combD.compile(optimizer=Adam(adam_lr, adam_beta_1), loss=['mse', 'mse', 'mae'], loss_weights=[1, 1, p_wgangp])
+#inter = merge([imageReal, imageFake], mode=RLinearMerge, output_shape=lambda d:d[0])
+#gradp = merge([inter, modelD(inter)], mode=GradientsPenalty, output_shape=lambda d:(d[0],1))
+combD = Model(inputs=[imageReal, imageFake], outputs=[DReal, DFake])
+combD.compile(optimizer=Adam(adam_lr, adam_beta_1), loss=['mse', 'mse'], loss_weights=[1, 1])
 
-p_beta_weight = K.variable(p_beta)
+p_recon_weight = K.variable(p_recon)
 
 def ConvGray(x):
 	return x[:,:,:,0:1] * 0.29891 + x[:,:,:,1:2] * 0.58661 + x[:,:,:,2:3] * 0.11448
+
+def GetDiffX(x): return K.max(K.abs(x[:,1:,:,:] - x[:,:-1,:,:]), -1)
+def GetDiffY(x): return K.max(K.abs(x[:,:,1:,:] - x[:,:,:-1,:]), -1)
 
 imageA = Input(shape=(imgsize,imgsize,1))
 modelD.trainable = False
 fake = modelG(imageA)
 disG = modelD(fake)
 regray = Lambda(ConvGray, output_shape=lambda d:d[:-1]+(1,))(fake)
-combM = Model(inputs=imageA, outputs=[disG, fake, regray])
-combM.compile(optimizer=Adam(adam_lr, adam_beta_1), loss=['mse', 'mae', 'mae'], loss_weights=[1,p_beta_weight,p_beta_weight])
+
+difffakeX = Lambda(GetDiffX, output_shape=lambda d:(d[0], d[1]-1, d[2], 1))(fake)
+diffgrayX = Lambda(GetDiffX, output_shape=lambda d:(d[0], d[1]-1, d[2], 1))(imageA)
+difffakeY = Lambda(GetDiffY, output_shape=lambda d:(d[0], d[1], d[2]-1, 1))(fake)
+diffgrayY = Lambda(GetDiffY, output_shape=lambda d:(d[0], d[1], d[2]-1, 1))(imageA)
+diffX = merge([difffakeX, diffgrayX], mode=lambda d:K.mean(K.relu(d[0]-d[1])), output_shape=lambda d:(d[0][0], 1))
+diffY = merge([difffakeY, diffgrayY], mode=lambda d:K.mean(K.relu(d[0]-d[1])), output_shape=lambda d:(d[0][0], 1))
+diff = add([diffX, diffY])
+combM = Model(inputs=imageA, outputs=[disG, fake, regray, diff])
+combM.compile(optimizer=Adam(adam_lr, adam_beta_1), loss=['mse', imgloss, 'mae', 'mae'], loss_weights=[1,p_recon_weight,p_gray,p_diff])
 
 def ResizeRatio(w, h):
 	short, long = min(w, h), max(w, h)
-	if short <= imgsize: obj = random.randint(imgsize+1, imgsize+10)
-	elif long >= short * 2.5: obj = random.randint(imgsize+1, imgsize+10)
-	elif imgsize < short < imgsize * 2: obj = random.randint(imgsize+1, short)
-	else: obj = random.randint(imgsize+1, int(imgsize * 2))
+	mxx = short
+	if short > 1500: mxx = short // 3
+	elif short > 1000: mxx = short // 2
+	if short <= imgsize: mxx = imgsize + 10
+	if long >= short * 2.5: mxx = imgsize + 10
+	obj = random.randint(imgsize+5, mxx)
 	ratio = obj / short
-	if ratio < 0.3: ratio = 0.3
 	return ratio
 
 def ImgGenerator(imgdir):
@@ -116,13 +127,14 @@ def ImgGenerator(imgdir):
 				w, h = img.shape[1], img.shape[0]
 				ratio = ResizeRatio(w, h)
 				img = cv2.resize(img, (int(w*ratio+.5), int(h*ratio+.5)))
+				if random.random() < 0.4: img = img[:,::-1,:]
 				gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-				img, gray = (img - 127.5)/127.5, (gray - 127.5)/127.5
+				img, gray = img / 127.5 - 1, gray / 127.5 - 1
 				gray = np.expand_dims(np.expand_dims(gray, 0), 3)
 				img = np.expand_dims(img, 0)
 				cache.append( (gray, img) )
-				if len(cache) > 400:
-					for ii in range(7000):
+				if len(cache) > 300:
+					for ii in range(3000):
 						gimg, cimg = random.choice(cache)
 						px, py = random.randint(0, cimg.shape[1]-imgsize), random.randint(0, cimg.shape[2]-imgsize)
 						gx, cx = gimg[:,px:px+imgsize,py:py+imgsize,:], cimg[:,px:px+imgsize,py:py+imgsize,:] 
@@ -141,8 +153,9 @@ def ImgGeneratorS(imgdir):
 		w, h = img.shape[1], img.shape[0]
 		ratio = ResizeRatio(w, h)
 		img = cv2.resize(img, (int(w*ratio+.5), int(h*ratio+.5)))
+		if random.random() < 0.4: img = img[:,::-1,:]
 		gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-		img, gray = (img - 127.5)/127.5, (gray - 127.5)/127.5
+		img, gray = img / 127.5 - 1, gray / 127.5 - 1
 		gray = np.expand_dims(np.expand_dims(gray, 0), 3)
 		img = np.expand_dims(img, 0)
 		for ii in range(10):
@@ -159,40 +172,43 @@ gen     = ImgGenerator(imgdirA)
 testgen = ImgGeneratorS(testimgdirA)
 
 #nb_batches = len(os.listdir(imgdirA)) // batch_size
-nb_batches = 2999
+nb_batches = 999
 ones  = np.ones(  (batch_size, imgsize//16, imgsize//16, 1) )
 zeros = np.zeros( (batch_size, imgsize//16, imgsize//16, 1) )
 
-
 record = []
-objD = [np.concatenate([ones]*batch_size,0), np.concatenate([zeros]*batch_size,0), np.zeros((batch_size,1))]
+objD = [np.concatenate([ones]*batch_size,0), np.concatenate([zeros]*batch_size,0), np.zeros((batch_size,1))][:2]
 for epoch in range(nb_epochs):
-	print('Epoch %d of %d' % (epoch+1, nb_epochs))
-	progress_bar = Progbar(target=nb_batches)
+	print('Epoch %d of %d' % (epoch, nb_epochs))
 	
+	D_iter = 1;  G_iter = 1;
+	if epoch > 20: D_iter = 7; G_iter = 3
+	progress_bar = Progbar(target=nb_batches*(G_iter+D_iter))
+	iter = 1
 	for index in range(nb_batches):
-		D_iter = 2
-		lossD = 0; 
 		for _ in range(D_iter):
 			gimg, cimg = next(gen)
 			generate = modelG.predict_on_batch(gimg)
 			record.append(generate)
-			if len(record) > 50: record = record[-25:]
-			lossD += combD.train_on_batch([cimg, random.choice(record)], objD)[0] / D_iter
+			if len(record) > 100: record = record[-50:]
+			lossD = combD.train_on_batch([cimg, random.choice(record)], objD)[0]
+			progress_bar.update(iter, values=[('D',lossD)])
+			iter += 1
 
-		for _ in range(1):
+		for _ in range(G_iter):
 			gimg, cimg = next(gen)
-			__, lossG, lossR, lossGray = combM.train_on_batch(gimg, [ones, cimg, gimg])
-			
-		progress_bar.update(index+1, values=[('D',lossD),('G',lossG),('R',lossR),('Gray',lossGray)])
-	
-	p_beta = np.clip(p_beta * 0.9, 0.1, 10)
-	K.set_value(p_beta_weight, p_beta)
+			__, lossG, lossR, lossGray, lossDiff = combM.train_on_batch(gimg, [ones, cimg, gimg, np.zeros((batch_size, 1))])
+			progress_bar.update(iter, values=[('G',lossG),('R',lossR),('Gray',lossGray),('Diff',lossDiff)])
+			iter += 1
 
-	print('Testing for epoch {}:'.format(epoch + 1))
-	print('p_beta=%.3f' % p_beta)
 	
-	if epoch % 5 == 4:
+	p_recon = np.clip(p_recon * 0.9, 1, 10)
+	K.set_value(p_recon_weight, p_recon)
+
+	print('Testing for epoch {}:'.format(epoch))
+	print('p_recon=%.3f' % p_recon)
+	
+	if epoch % 4 == 3:
 		lr =  K.get_value(combD.optimizer.lr) * 0.9
 		print('lr=%.8f' % lr)
 		K.set_value( combD.optimizer.lr, lr )
@@ -207,11 +223,12 @@ for epoch in range(nb_epochs):
 	tGray = np.concatenate( graytest, axis=0 ) 
 	tCol = np.concatenate( colortest, axis=0 ) 
 	tGen = modelG.predict(tGray, batch_size=1)
-	obj = [np.concatenate([ones]*numtest,0), np.concatenate([zeros]*numtest,0), np.zeros((numtest,1))]
-	_, lossReal, lossFake, lossGP = combD.evaluate([tCol, tGen], obj, batch_size=1, verbose=0)
-	_, lossG, lossR, lossGray = combM.evaluate(tGray, [obj[0], tCol, tGray], batch_size=1, verbose=0)
-	print('lossReal=%.4f, lossFake=%.4f, lossGP=%.4f' % (lossReal, lossFake, lossGP))
-	print('lossG=%.4f, lossR=%.4f, lossGray=%.4f' % (lossG, lossR, lossGray))
+	obj = [np.concatenate([ones]*numtest,0), np.concatenate([zeros]*numtest,0), np.zeros((numtest,1))][:2]
+	dloss = combD.evaluate([tCol, tGen], obj, batch_size=1, verbose=0)
+	mloss = combM.evaluate(tGray, [obj[0], tCol, tGray, np.zeros((numtest, 1))], batch_size=1, verbose=0)
+	dloss += (0,)
+	print('lossReal=%.4f, lossFake=%.4f, lossGP=%.4f' % (dloss[1], dloss[2], dloss[3]))
+	print('lossG=%.4f, lossR=%.4f, lossGray=%.4f, lossDiff=%.4f' % (mloss[1], mloss[2], mloss[3], mloss[4]))
 	tGray = np.repeat(tGray,3)
 	tGray = tGray.reshape(-1,imgsize,channels)
 	tCol = tCol.reshape(-1,imgsize,channels)
--- model.py
+++ model.py
@@ -2,14 +2,31 @@
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import *
 from keras.models import *
-from keras.initializers import RandomNormal
+from keras.initializers import RandomNormal, Ones, Zeros
 import keras.backend as K
 import tensorflow as tf
 
 
-def InstanceNorm(x):
-	mean, var = tf.nn.moments(x, axes=[1,2])
-	return (x - mean) * tf.rsqrt(var + 1e-7)
+class InstanceNormalization(Layer):
+	def __init__(self, axis=-1, epsilon=1e-7, **kwargs):
+		super(InstanceNormalization, self).__init__(**kwargs)
+		self.axis = axis
+		self.epsilon = epsilon
+
+	def build(self, input_shape):
+		dim = input_shape[self.axis]
+		if dim is None:
+			raise ValueError('Axis '+str(self.axis)+' of input tensor should have a defined dimension but the layer received an input with shape '+str(input_shape)+ '.')
+		shape = (dim,)
+
+		self.gamma = self.add_weight(shape=shape, name='gamma', initializer='ones')
+		self.beta = self.add_weight(shape=shape, name='beta', initializer='zeros')
+		self.built = True
+
+	def call(self, inputs, training=None):
+		mean, var = tf.nn.moments(inputs, axes=[1,2], keep_dims=True)
+		return K.batch_normalization(inputs, mean, var, self.beta, self.gamma, self.epsilon)
+
 
 def ReflectPad(x, ks=1):
 	return tf.pad(x, [[0,0],[ks,ks],[ks,ks],[0,0]], "REFLECT")
@@ -63,19 +80,24 @@ def BuildGenerator(img):
 def BuildDiscriminator(img):
 	ddim = 64
 	x = Conv2D(ddim, 7, padding='same', strides=2)(img)
-	x = Lambda(InstanceNorm)(x)
+	x = InstanceNormalization()(x)
 	x = LeakyReLU()(x)
+	x = Dropout(.5)(x)
 	x = Conv2D(ddim*2, 3, padding='same', strides=2)(x)
-	x = Lambda(InstanceNorm)(x)
+	x = InstanceNormalization()(x)
 	x = LeakyReLU()(x)
+	x = Dropout(.5)(x)
 	x = Conv2D(ddim*4, 3, padding='same', strides=2)(x)
-	x = Lambda(InstanceNorm)(x)
+	x = InstanceNormalization()(x)
 	x = LeakyReLU()(x)
+	x = Dropout(.5)(x)
 	x = Conv2D(ddim*8, 3, padding='same', strides=2)(x)
-	x = Lambda(InstanceNorm)(x)
+	x = InstanceNormalization()(x)
 	x = LeakyReLU()(x)
+	x = Dropout(.5)(x)
 	x = Conv2D(ddim*8, 3, padding='same', strides=1)(x)
-	x = Lambda(InstanceNorm)(x)
+	x = InstanceNormalization()(x)
 	x = LeakyReLU()(x)
+	x = Dropout(.5)(x)
 	x = Conv2D(1, 3, padding='same', strides=1)(x)
 	return Model(inputs=img, outputs=x)
